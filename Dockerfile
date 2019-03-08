b04d0f9344296032b6ba1f294f21c459ead3f309
Ballantyne, Bob <bob.ballantyne@emc.com>
2019-03-07 19:45:45 -0500

    reset history

--- Dockerfile
+++ Dockerfile
@@ -1,32 +1,30 @@
-ARG FROM_BASE=alpine:3.6
+ARG FROM_BASE=${DOCKER_REGISTRY:-ubuntu-s2:5000/}${CONTAINER_OS:-alpine}/php/${PHP5_VERSION:-5.6.37-r0}:${BASE_TAG:-latest}
 FROM $FROM_BASE
 
 # name and version of this docker image
-ARG CONTAINER_NAME=supervisord-monitor
-ARG CONTAINER_VERSION=1.0.0
+ARG CONTAINER_NAME=smonitor
+# Specify CBF version to use with our configuration and customizations
+ARG CBF_VERSION
 
-LABEL org_name=$CONTAINER_NAME \
-      version=$CONTAINER_VERSION 
+# include our project files
+COPY build Dockerfile /tmp/
 
 # set to non zero for the framework to show verbose action scripts
-ARG DEBUG_TRACE=0
-
-# Add CBF, configuration and customizations
-ARG CBF_VERSION=${CBF_VERSION:-v2.0}
-ADD "https://github.com/ballab1/container_build_framework/archive/${CBF_VERSION}.tar.gz" /tmp/
-COPY build /tmp/
+#    (0:default, 1:trace & do not cleanup; 2:continue after errors)
+ENV DEBUG_TRACE=0
 
 
 ARG SMONITOR_VERSION=v1.1.0
+LABEL version.smonitor=$SMONITOR_VERSION
 
 
 # build content
 RUN set -o verbose \
     && chmod u+rwx /tmp/build.sh \
-    && /tmp/build.sh "$CONTAINER_NAME"
+    && /tmp/build.sh "$CONTAINER_NAME" "$DEBUG_TRACE"
 RUN [ $DEBUG_TRACE != 0 ] || rm -rf /tmp/* 
 
 
 ENTRYPOINT [ "docker-entrypoint.sh" ]
 #CMD ["$CONTAINER_NAME"]
-CMD ["supervisord-monitor"]
+CMD ["smonitor"]
--- build/build.sh
+++ build/build.sh
@@ -1,15 +1,89 @@
-#!/bin/bash
+#!/bin/sh
+
+die() {
+    echo "$1"
+    exit 1
+}
+
+osname() {
+    if [ -e /etc/os-release ]; then
+        cat /etc/os-release | grep -E '^ID=' 2>/dev/null | awk -F '=' '{print $2}'
+    else
+        echo mingw64
+    fi
+}
+
+echo '= Environment ======================================='
+printf '    %s\n' $(env | sort)
+echo '= Commandline Params ================================'
+printf '    %s\n' "$@"
+echo '= Variables used ===================================='
+echo "    CBF_VERSION: .${CBF_VERSION}."
+echo '====================================================='
+
+case "$(osname)" in
+    alpine)
+        # ensure we have bash support
+        if [ -z "$(which bash)" ]; then
+            apk update
+            apk add --no-cache bash ca-certificates openssl
+        fi;;
+    centos|fedora)
+        # ensure we have wget support
+        if [ ! -e /usr/bin/wget ]; then
+            yum update
+            yum install -y wget ca-certificates openssl
+        fi;;
+    ubuntu)
+        # ensure we have wget support
+        if [ -z "$(which wget)" ]; then
+            apt-get update
+            apt-get install -y apt-utils wget ca-certificates openssl
+        fi;;
+esac
 
 cd /tmp
-declare cbf_dir=/tmp/container_build_framework
+cbf_dir=/tmp/container_build_framework
 
-if [ ! -d "$cbf_dir" ]; then
+# check if we need to download CBF
+if [ -d "$cbf_dir" ]; then
+    echo "Using local build version of CBF"
+    find /tmp -name 'C?F.properties' -delete
 
-    declare cbf_tar=$( ls *.tar.gz )
-    [ -z "$cbf_tar" ] || tar xzf "${cbf_tar}"
+elif [ "$CBF_VERSION" ]; then
+    # since no CBF directory located, attempt to download CBF based on specified verion
+    CBF_TGZ=/tmp/cbf.tar.gz
+    CBF_URL="https://github.com/ballab1/container_build_framework/archive/${CBF_VERSION}.tar.gz"
+    echo "Downloading CBF:$CBF_VERSION from $CBF_URL"
 
-    cbf_dir="$( ls -d container_build_framework-* )"
-    [ "$cbf_dir" ] || exit 1
+    if [ -e /usr/bin/wget ]; then
+        wget --no-check-certificate --quiet --output-document="$CBF_TGZ" "$CBF_URL" || die "Failed to download $CBF_URL"
+
+    elif [ -e /usr/bin/curl ]; then
+        curl --insecure --silent --output "$CBF_TGZ" "$CBF_URL" || die "Failed to download $CBF_URL"
+
+    else
+        die "Neither wget or curl is installed to download cbf from $CBF_URL"
+    fi
+
+    echo 'Unpacking downloaded copy of CBF'
+    tar --exclude 'C?F.properties' -xzf "$CBF_TGZ" || die "Failed to unpack $CBF_TGZ"
+    cbf_dir="$( ls -d container_build_framework* 2>/dev/null )"
+
+else
+    echo 'Unpacking stashed copy of CBF'
+
+    # setup pointer to archive from prior build
+    CBF_TGZ=/usr/local/crf/cbf.tar.gz
+    mkdir -p "$cbf_dir" || die 'Unable to unpack stashed CBF'
+    tar --exclude 'C?F.properties'  -xzf "$CBF_TGZ" -C "$cbf_dir" || die "Failed to unpack $CBF_TGZ"
 fi
+
+
+# verify CBF directory exists
+[ "$cbf_dir" ] && [ -d "$cbf_dir" ] ||  die 'No framework directory located'
+
+
 echo "loading framework from ${cbf_dir}"
-source "${cbf_dir}/bin/build.sh" "$@"
+chmod a+rx "${cbf_dir}/cbf/bin/build.sh"
+exec "${cbf_dir}/cbf/bin/build.sh" "$@"
